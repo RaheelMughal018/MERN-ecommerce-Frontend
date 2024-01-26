815060861a3de43399948e7ee4576126d703fafe
RaheelMughal018 <raheelmughal018>
2024-01-26 05:06:41 +0500

    filtering func issue

--- src/features/product/productAPI.js
+++ src/features/product/productAPI.js
@@ -1,9 +1,8 @@
 export function fetchAllProducts() {
   // we will not hard-cord Server URL here
-  return new Promise(async (resolve) =>{
-    const response = await fetch('http://localhost:3000/products') 
-    const data = await response.json()
-    resolve({data})
-  }
-  );
+  return new Promise(async (resolve) => {
+    const response = await fetch("http://localhost:8080/products");
+    const data = await response.json();
+    resolve({ data });
+  });
 }
