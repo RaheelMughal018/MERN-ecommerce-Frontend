6c8486d6ed714a58aa0462806493a436a9a546d8
Raheel Mughal <Raheelmughal018@gmail.com>
2024-01-30 01:27:34 +0500

    working on detail page

---
+++ src/App.js
@@ -0,0 +1,51 @@
+import "./App.css";
+import HomePage from "./pages/HomePage";
+import LoginPage from "./pages/LoginPage";
+import SignupPage from "./pages/SignupPage";
+import ProductDetailPage from "./pages/ProductDetailPage";
+
+import {
+  createBrowserRouter,
+  RouterProvider,
+  Route,
+  Link,
+} from "react-router-dom";
+import CartPage from "./pages/CartPage";
+import Checkout from "./pages/CheckoutPage";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <HomePage />,
+  },
+  {
+    path: "/sign-up",
+    element: <SignupPage />,
+  },
+  {
+    path: "/login",
+    element: <LoginPage />,
+  },
+  {
+    path: "/cart",
+    element: <CartPage />,
+  },
+  {
+    path: "/checkout",
+    element: <Checkout />,
+  },
+  {
+    path: "/product-detail/:id",
+    element: <ProductDetailPage />,
+  },
+]);
+
+function App() {
+  return (
+    <div className="App">
+      <RouterProvider router={router} />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/pages/CartPage.jsx
@@ -0,0 +1,10 @@
+import Cart from "../features/cart/Cart";
+import NavBar from "../features/navbar/Navbar";
+
+export default function CartPage() {
+  return (
+    <NavBar>
+      <Cart />
+    </NavBar>
+  );
+}
